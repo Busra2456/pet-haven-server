07ef0de8d9092f4cbadac51726013d901ef17f75
Busra2456 <you@example.com>
2025-11-02 11:19:58 +0600

    home menu order is completed server

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+ 
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9hnuiul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const usersCollection = client.db("PetHaven").collection("users")
+
+      const reviewsCollection = client.db("PetHaven").collection("reviews")
+
+
+
+
+    app.get("/users", async(req,res) =>{
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+    
+
+
+    app.get("/reviews", async(req,res) =>{
+      const result = await reviewsCollection.find().toArray();
+      res.send(result)
+    })
+
+     
+ 
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+//     await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req,res) =>{
+      res.send('PetHaven is setting')
+})
+
+app.listen(port, () =>{
+      console.log(`Pet Haven is setting on port ${port}`);
+})
